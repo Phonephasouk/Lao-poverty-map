a1dfef48da9a1d5426c00eadd86e663fd920b3e4
Noi Phonephasouk <38683669+Phonephasouk@users.noreply.github.com>
2019-02-21 16:23:22 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var isMobile = false; //initiate as false
-// device detection
-if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
+		// device detection
+		if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|ipad|iris|kindle|Android|Silk|lge |maemo|midp|mmp|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows (ce|phone)|xda|xiino/i.test(navigator.userAgent) 
 			|| /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(navigator.userAgent.substr(0,4))) isMobile = true;
 		
 		
@@ -23,7 +23,9 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 			},'Zoom To Home', {
 			position: 'bottomleft'
 		}).addTo(m);
+
 		
+
 		var OpenCartoMap = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}'+ (L.Browser.retina ? '@2x.png' : '.png'),{
 				attribution:'Basemap data &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> | Basemap style &copy; <a href="https://carto.com/attributions">CARTO</a>',
 				subdomains: 'abcd',
@@ -171,6 +173,7 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 		};
 
 
+
 		var cmap_poverty = [{"label" : ">&nbsp;87.0", "lower" : 87.0, "fill" : '#990000'},
 						{"label" : "75.0&nbsp;-&nbsp;87.0", "lower" : 75.0, "fill" : '#d7301f'},
 						{"label" : "62.0&nbsp;-&nbsp;75.0", "lower" : 62.0, "fill" : '#ef6548'},
@@ -233,51 +236,6 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 						{"label" : "25.0&nbsp;-&nbsp;37.0", "lower" : 25.0, "fill" : '#ccece6'},
 						{"label" : "12.0&nbsp;-&nbsp;25.0", "lower" : 12.0, "fill" : '#e5f5f9'},
 						{"label" : "<&nbsp;12.0", "lower" : 0.0,  "fill" : '#f7fcfd'}];
-
-		/*var cmap_sanitation = [{"label" : "", "lower" : 7, "fill" : '#005a32'},
-						   {"label" : "", "lower" : 6, "fill" : '#238443'},
-						   {"label" : "", "lower" : 5, "fill" : '#41ab5d'},
-						   {"label" : "", "lower" : 4, "fill" : '#78c679'},
-						   {"label" : "", "lower" : 3, "fill" : '#addd8e'},
-						   {"label" : "", "lower" : 2, "fill" : '#d9f0a3'},
-						   {"label" : "", "lower" : 1, "fill" : '#f7fcb9'},
-						   {"label" : "", "lower" : 0, "fill" : '#ffffe5'}];
-
-		var cmap_water= [{"label" : "", "lower" : 7, "fill" : '#0c2c84'},
-						   {"label" : "", "lower" : 6, "fill" : '#225ea8'},
-						   {"label" : "", "lower" : 5, "fill" : '#1d91c0'},
-						   {"label" : "", "lower" : 4, "fill" : '#41b6c4'},
-						   {"label" : "", "lower" : 3, "fill" : '#7fcdbb'},
-						   {"label" : "", "lower" : 2, "fill" : '#c7e9b4'},
-						   {"label" : "", "lower" : 1, "fill" : '#edf8b1'},
-						   {"label" : "", "lower" : 0, "fill" : '#ffffd9'}];
-
-		var cmap_electricity = [{"label" : "", "lower" : 7, "fill" : '#8c2d04'},
-						  {"label" : "", "lower" : 6, "fill" : '#cc4c02'},
-						  {"label" : "", "lower" : 5, "fill" : '#ec7014'},
-						  {"label" : "", "lower" : 4, "fill" : '#fe9929'},
-						  {"label" : "", "lower" : 3, "fill" : '#fec44f'},
-						  {"label" : "", "lower" : 2, "fill" : '#fee391'},
-						  {"label" : "", "lower" : 1, "fill" : '#fff7bc'},
-						  {"label" : "", "lower" : 0, "fill" : '#ffffe5'}];
-						  
-		var cmap_tphone = [{"label" : "", "lower" : 7, "fill" : '#b10026'},
-						  {"label" : "", "lower" : 6, "fill" : '#e31a1c'},
-						  {"label" : "", "lower" : 5, "fill" : '#fc4e2a'},
-						  {"label" : "", "lower" : 4, "fill" : '#fd8d3c'},
-						  {"label" : "", "lower" : 3, "fill" : '#feb24c'},
-						  {"label" : "", "lower" : 2, "fill" : '#fed976'},
-						  {"label" : "", "lower" : 1, "fill" : '#ffeda0'},
-						  {"label" : "", "lower" : 0, "fill" : '#ffffcc'}];
-						  
-		var cmap_urban = [{"label" : "", "lower" : 7, "fill" : '#081D58'},
-						  {"label" : "", "lower" : 6, "fill" : '#243996'},
-						  {"label" : "", "lower" : 5, "fill" : '#206CAE'},
-						  {"label" : "", "lower" : 4, "fill" : '#2CA0C1'},
-						  {"label" : "", "lower" : 3, "fill" : '#64C3BE'},
-						  {"label" : "", "lower" : 2, "fill" : '#B2E0B6'},
-						  {"label" : "", "lower" : 1, "fill" : '#E8F6B1'},
-						  {"label" : "", "lower" : 0, "fill" : '#FFFFD9'}];*/
 		function get_var(legX) {
 
 		  if (legX == "poverty")    return "Poverty_He";
@@ -367,6 +325,7 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 		function fileFilter(x)   { return x.file   == this; };
 		function methodFilter(x) { return x.method == this; };
 		function seedFilter(x)   { return x.seed   == this; };		
+
 			
 		//coloring var	
 		var cmap = {"poverty" : cmap_poverty,
@@ -392,17 +351,37 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 		};
 
 		//function for layer style based on coloring on cmap
+		function styleD(feature) {
+			val = parseFloat(feature.properties[variable]);
+			return {
+				weight: 1.2,
+				opacity: 0.9,
+				color: 'black',
+				fillOpacity: 0.65,
+				fillColor: getColor(val)
+			};
+		};
 		function style(feature) {
 			val = parseFloat(feature.properties[variable]);
 			return {
 				weight: 1.2,
 				opacity: 0.9,
 				color: 'black',
-				fillOpacity: 0.75,
+				fillOpacity: 0.65,
 				fillColor: getColor(val)
 			};
 		};
-
+		function styleP(feature) {
+			val = parseFloat(feature.properties[variable]);
+			return {
+				weight: 3.5,
+				opacity: 0.9,
+				color: 'black',
+				fillOpacity: 0.65,
+				fillColor: getColor(val)
+			};
+		};
+	
 
 		//legend design
 		var legend = L.control({position: 'bottomright'});
@@ -445,7 +424,7 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 			repaint_map(variable);
 
 			//retrieve the range for legends case by case, by equal count
-			/*if (shading == "sanitation") {
+			if (shading == "sanitation") {
 				mini = 3, maxi = 0;
 				district_lay.eachLayer(function (layer) {
 					c = parseFloat(layer.feature.properties[variable])
@@ -523,7 +502,7 @@ if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine
 					cmap_electricity[7-s]["label"] = (mini + s * step_size).toFixed(1) + "&nbsp;-&nbsp;" + (mini + (s+1) * step_size).toFixed(1);
 				}
 			  cmap_electricity[7]["lower"] = mini;
-			}*/
+			}
 			
 			var labels = [];
 			if (shading in cmap) {
